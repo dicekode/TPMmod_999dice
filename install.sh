630233ddb9e956dbb10d5c2d53853677f4aa40c3
Cyjan <MrCyjaneK@Gmail.com>
2019-08-25 21:26:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+echo "Installing php extensions";
+sudo apt-get install php-pear php-zip php-curl php-gd php-mysql php-xml php-mbstring -y
 echo "Installing composer";
 curl -sS https://getcomposer.org/installer | php
 echo "Installing 999dice.";
