cd418e97f1a00c9bdce530523717a711a9cd01e7
Cyjan <MrCyjaneK@Gmail.com>
2019-08-28 11:44:01 +0300

    Added unzip

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 cd "/home/`whoami`/TPM/mods/999dice"
+echo "Installing unzip";
+sudo apt install unzip -y
 echo "Installing php extensions";
 sudo apt-get install php-pear php-zip php-curl php-gd php-mysql php-xml php-mbstring -y
 echo "Installing composer";
