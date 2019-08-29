6f752c8fc6da0a5fa3610ae637b1fd63556b54a0
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 00:16:59 +0200

    It's better to start server in right directory...

--- start.sh
+++ start.sh
@@ -13,5 +13,6 @@ else
 fi
 echo "==== core.php started ====";
 echo "==== api starting ====";
+cd /home/`whoami`/TPM/mods/999dice/api
 screen -dm php -S 127.0.0.1:`cat port.txt`
 echo "==== api started ====";
