608f791025b869bf28ac697747dcdff4f8e3168d
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 00:21:52 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,6 +2,7 @@
 try {
 $f = "./todo.txt";
 $g = "./reply.txt";
+print_r($_GET);
 if ($_GET['action'] == 'bet') {
     file_put_contents($f,"bet\n".$_GET['amount']."\n".$_GET['chance']."\n".$_GET['bethi']);
     $try = 0;
