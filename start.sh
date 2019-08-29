9ae5812f5b4902c477d5e2c8e95c57d843f5ab31
Cyjan <MrCyjaneK@Gmail.com>
2019-08-29 23:56:47 +0200

    check if login info is correct and api start

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 echo "==== Starting core.php====";
 cd /home/`whoami`/TPM/mods/999dice
-php core.php
-ec=$?
-echo "==== core.php exited ====";
-exit $ec
+php core.php test
+if [ $? == 0 ];
+then
+    screen -dm php core.php
+else
+    echo "Oups, sombody forget to configure 999dice";
+    bash config.sh
+    bash start.sh
+fi
+echo "==== core.php started ====";
+echo "==== api starting ====";
+screen -dm php -S 127.0.0.1:`cat port.txt`
+echo "==== api started ====";
