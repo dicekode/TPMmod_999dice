eb1a9be6c3d4e18aa26398a3e4c9acf9ed3fddeb
Cyjan <MrCyjaneK@Gmail.com>
2019-08-30 14:47:23 +0200

    Switched from reilag's to MrCyjaneK's 999dice module

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get install php-pear php-zip php-curl php-gd php-mysql php-xml php-mbst
 echo "Installing composer";
 curl -sS https://getcomposer.org/installer | php
 echo "Installing 999dice.";
-php composer.phar require reilag/999dice
+php composer.phar require MrCyjaneK/999dice
 # echo "Installing react";
 # php composer.phar require react/react:^1.0
 echo "All done"
